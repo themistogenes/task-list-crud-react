929434e0a9487e6928d18b3b981ca2e07baf8af7
themistogenes <85630327+themistogenes@users.noreply.github.com>
2023-08-09 18:58:21 -0400

    Initial commit

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,26 @@
+// component import
+import TaskItem from './TaskItem'
+
+// styles module (scoped to this specific component)
+import styles from './TaskList.module.css'
+
+const TaskList = ({ tasks, deleteTask, toggleTask, enterEditMode }) => {
+
+  return (
+    <ul className={styles.tasks}>
+      {tasks.sort((a, b) => b.id - a.id).map(task => (
+        <TaskItem
+          key={task.id}
+          task={task}
+          deleteTask={deleteTask}
+          toggleTask={toggleTask}
+          enterEditMode={enterEditMode}
+        />
+      ))
+      }
+    </ul>
+  )
+
+}
+
+export default TaskList
